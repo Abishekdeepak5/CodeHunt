bbbf817a29061070eb6491acea6746786c8aa4ff
Abishekdeepak5 <143536892+Abishekdeepak5@users.noreply.github.com>
2024-10-15 16:50:11 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,18 @@
+/*
 document.getElementById('round1-link').addEventListener('click', function() {
-    window.location.href = 'https://www.hackerrank.com/gce-sympo'; // Replace with actual link
+    window.location.href = 'https://www.hackerrank.com/gce-sympo';
 });
 
 document.getElementById('round2-link').addEventListener('click', function() {
-    window.location.href = 'https://www.hackerrank.com/sparzocodedebugging'; // Replace with actual link
+    window.location.href = 'https://www.hackerrank.com/sparzocodedebugging';
 });
 
 document.getElementById('round3-link').addEventListener('click', function() {
-    window.location.href = '#'; // Replace with actual link
+    window.location.href = '#'; 
 });
 
 document.getElementById('round4-link').addEventListener('click', function() {
-    window.location.href = '#'; // Replace with actual link
+    window.location.href = '#'; 
 });
+
+*/
